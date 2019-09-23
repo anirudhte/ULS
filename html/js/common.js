bdacb21e2583dfe90bd2dabcb678650d775fd124
anirudhte <55486520+anirudhte@users.noreply.github.com>
2019-09-23 18:31:27 +0530

    js added

---
+++ html/js/common.js
@@ -0,0 +1,23 @@
+
+$(document).ready(function(){
+
+$(' aside .aside-container li.active').addClass('open').children('ul').show();
+	$('aside .aside-container li.has-sub>a').on('click', function(){
+		//$(this).removeAttr('href');
+		var element = $(this).parent('li');
+		if (element.hasClass('open')) {
+			element.removeClass('open');
+			element.find('li').removeClass('open');
+			element.find('ul').slideUp(200);
+		}
+		else {
+			element.addClass('open')
+			element.children('ul').slideDown(200);
+			element.siblings('li').children('ul').slideUp(200);
+			element.siblings('li').removeClass('open');
+			element.siblings('li').find('li').removeClass('open');
+			element.siblings('li').find('ul').slideUp(200);
+		}
+	});
+
+});
